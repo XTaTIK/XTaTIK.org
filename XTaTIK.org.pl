97ddd14ed6cc7979284b13f666f602f032a5746b
Zoffix Znet <cpan@zoffix.com>
2015-04-23 11:14:16 -0400

    first commit

---
+++ XTaTIK.org.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+
+use Mojolicious::Lite;
+plugin 'AssetPack';
+
+app->secrets([42]);
+app->config(hypnotoad => {listen => ['http://*:8081']});
+app->asset('app.css' => qw{/main.scss  sprites:///s/} );
+
+
+require 'data.html';
+push @{ app->renderer->classes }, 'Fake';
+
+#### ROUTES
+
+get '/' => sub {
+    my $c = shift;
+} => 'index';
+
+any '/sitemap.xml' => sub {
+    my $c = shift;
+    $c->stash( format => 'xml' );
+} => 'sitemap';
+
+app->start;
